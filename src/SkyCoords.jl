1313d8c8c66d2ddbd6ca92b1fa2841dc475fed3a
Kyle Barbary <kylebarbary@gmail.com>
2015-04-14 21:26:18 -0700

    code organization

--- src/SkyCoords.jl
+++ src/SkyCoords.jl
@@ -9,6 +9,33 @@ export AbstractSkyCoords,
 import Base: convert
 
 # -----------------------------------------------------------------------------
+# Types
+
+abstract AbstractSkyCoords
+
+immutable ICRSCoords <: AbstractSkyCoords
+    ra::Float64
+    dec::Float64
+    ICRSCoords(ra::Real, dec::Real) = @compat new(mod(Float64(ra), 2pi),
+                                                  Float64(dec))
+end
+
+immutable GalCoords <: AbstractSkyCoords
+    l::Float64
+    b::Float64
+    GalCoords(l::Real, b::Real) = @compat new(mod(Float64(l), 2pi),
+                                              Float64(b))
+end
+
+# FK5 is parameterized by equinox (e)
+immutable FK5Coords{e} <: AbstractSkyCoords
+    ra::Float64
+    dec::Float64
+    FK5Coords(ra::Real, dec::Real) = @compat new(mod(Float64(ra), 2pi),
+                                                 Float64(dec))
+end
+
+# -----------------------------------------------------------------------------
 # Helper functions: Immutable array operations
 
 # We use immutable array operations to avoid allocating memory for
@@ -91,16 +118,47 @@ end
 # [x, y, z] unit vector -> (lon, lat)
 cart2coords(v) = atan2(v.y, v.x), atan2(v.z, sqrt(v.x*v.x + v.y*v.y))
 
+# -----------------------------------------------------------------------------
+# Constant rotation matricies and precession matrix function
+
+# ICRS --> FK5 at J2000 (See USNO Circular 179, section 3.5)
+eta0 = deg2rad(-19.9 / 3600000.)
+xi0 = deg2rad(9.1 / 3600000.)
+da0 = deg2rad(-22.9 / 3600000.)
+const ICRS_TO_FK5J2000 = xrotmat(-eta0) * yrotmat(xi0) * zrotmat(da0)
+const FK5J2000_TO_ICRS = ICRS_TO_FK5J2000'
+
+# FK5J2000 --> Gal
+# Note that galactic pole and zeropoint of l are somewhat arbitrary
+# and not officially defined (as far as I know). The values below are
+# from astropy.coordinates, which includes the following comment:
+# | "This gives better consistency with other codes than using the values
+# |  from Reid & Brunthaler 2004 and the best self-consistency between FK5
+# |  -> Galactic and FK5 -> FK4 -> Galactic. The lon0 angle was found by
+# |  optimizing the self-consistency."
+ngp_fk5j2000_ra = deg2rad(192.8594812065348)
+ngp_fk5j2000_dec = deg2rad(27.12825118085622)
+lon0_fk5j2000 = deg2rad(122.9319185680026)
+const FK5J2000_TO_GAL = (zrotmat(pi - lon0_fk5j2000) *
+                         yrotmat(pi/2. - ngp_fk5j2000_dec) *
+                         zrotmat(ngp_fk5j2000_ra))
+const GAL_TO_FK5J2000 = FK5J2000_TO_GAL'
+
+# Gal --> ICRS: simply chain through FK5J2000 
+const GAL_TO_ICRS = FK5J2000_TO_ICRS * GAL_TO_FK5J2000
+const ICRS_TO_GAL = GAL_TO_ICRS'
+
+# FK5J2000 --> FK5{epoch}
 # Computes the precession matrix from J2000 to the given Julian equinox.
 # Expression from from Capitaine et al. 2003 as expressed in the USNO
 # Circular 179.  This should match the IAU 2006 standard from SOFA.
-const pzeta =  [ 2.650545, 2306.083227,  0.2988499,  0.01801828, -0.000005971,
-                -0.0000003173]
-const pz =     [-2.650545, 2306.077181,  1.0927348,  0.01826837, -0.000028596,
-                -0.0000002904]
-const ptheta = [      0.0, 2004.191903, -0.4294934, -0.04182264, -0.000007089,
+const pzeta = [2.650545, 2306.083227, 0.2988499, 0.01801828, -0.000005971,
+               -0.0000003173]
+const pz = [-2.650545, 2306.077181, 1.0927348, 0.01826837, -0.000028596,
+            -0.0000002904]
+const ptheta = [0.0, 2004.191903, -0.4294934, -0.04182264, -0.000007089,
                 -0.0000001274]
-function precess_from_j2000_capitaine(equinox)
+function precess_from_j2000(equinox)
     t = (equinox - 2000.0) / 100.0
     tn = 1.0
     zeta = pzeta[1]
@@ -118,60 +176,8 @@ function precess_from_j2000_capitaine(equinox)
     zrotmat(-z) * yrotmat(theta) * zrotmat(-zeta)
 end
 
-# Constants -------------------------------------------------------------------
-
-# ICRS --> FK5 at J2000 (See USNO Circular 179, section 3.5)
-eta0 = deg2rad(-19.9 / 3600000.)
-xi0 = deg2rad(9.1 / 3600000.)
-da0 = deg2rad(-22.9 / 3600000.)
-const icrs_to_fk5j2000 = xrotmat(-eta0) * yrotmat(xi0) * zrotmat(da0)
-const fk5j2000_to_icrs = icrs_to_fk5j2000'
-
-# Galactic pole and zeropoint of l, from astropy.coordinates, which includes
-# the following note:
-# "This gives better consistency with other codes than using the values
-# from Reid & Brunthaler 2004 and the best self-consistency between FK5
-# -> Galactic and FK5 -> FK4 -> Galactic. The lon0 angle was found by
-# optimizing the self-consistency."
-ngp_fk5j2000_ra = deg2rad(192.8594812065348)
-ngp_fk5j2000_dec = deg2rad(27.12825118085622)
-lon0_fk5j2000 = deg2rad(122.9319185680026)
-const fk5j2000_to_gal = (zrotmat(pi - lon0_fk5j2000) *
-                         yrotmat(pi/2. - ngp_fk5j2000_dec) *
-                         zrotmat(ngp_fk5j2000_ra))
-const gal_to_fk5j2000 = fk5j2000_to_gal'
-
-# Gal -> ICRS
-const gal_to_icrs = fk5j2000_to_icrs * gal_to_fk5j2000
-const icrs_to_gal = gal_to_icrs'
-
-# Types ----------------------------------------------------------------------
-
-abstract AbstractSkyCoords
-
-immutable ICRSCoords <: AbstractSkyCoords
-    ra::Float64
-    dec::Float64
-    ICRSCoords(ra::Real, dec::Real) = @compat new(mod(Float64(ra), 2pi),
-                                                  Float64(dec))
-end
-
-immutable GalCoords <: AbstractSkyCoords
-    l::Float64
-    b::Float64
-    GalCoords(l::Real, b::Real) = @compat new(mod(Float64(l), 2pi),
-                                              Float64(b))
-end
-
-# FK5 is parameterized by equinox (e)
-immutable FK5Coords{e} <: AbstractSkyCoords
-    ra::Float64
-    dec::Float64
-    FK5Coords(ra::Real, dec::Real) = @compat new(mod(Float64(ra), 2pi),
-                                                 Float64(dec))
-end
-
-# Convert methods -------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Type-dependent methods
 
 # Abstract away specific field names (ra, dec vs l, b)
 coords2cart(c::ICRSCoords) = coords2cart(c.ra, c.dec)
@@ -179,34 +185,33 @@ coords2cart(c::GalCoords) = coords2cart(c.l, c.b)
 coords2cart{e}(c::FK5Coords{e}) = coords2cart(c.ra, c.dec)
 
 # Rotation matrix between coordinate systems: `rotmat(to, from)`
-rotmat(::Type{GalCoords}, ::Type{ICRSCoords}) = icrs_to_gal
-rotmat(::Type{ICRSCoords}, ::Type{GalCoords}) = gal_to_icrs
+rotmat(::Type{GalCoords}, ::Type{ICRSCoords}) = ICRS_TO_GAL
+rotmat(::Type{ICRSCoords}, ::Type{GalCoords}) = GAL_TO_ICRS
 rotmat{e}(::Type{FK5Coords{e}}, ::Type{ICRSCoords}) =
-    precess_from_j2000_capitaine(e) * icrs_to_fk5j2000
+    precess_from_j2000(e) * ICRS_TO_FK5J2000
 rotmat{e}(::Type{FK5Coords{e}}, ::Type{GalCoords}) =
-   precess_from_j2000_capitaine(e) * gal_to_fk5j2000
+   precess_from_j2000(e) * GAL_TO_FK5J2000
 rotmat{e}(::Type{ICRSCoords}, ::Type{FK5Coords{e}}) = 
-    fk5j2000_to_icrs * precess_from_j2000_capitaine(e)'
+    FK5J2000_TO_ICRS * precess_from_j2000(e)'
 rotmat{e}(::Type{GalCoords}, ::Type{FK5Coords{e}}) =
-    fk5j2000_to_gal * precess_from_j2000_capitaine(e)'
+    FK5J2000_TO_GAL * precess_from_j2000(e)'
 rotmat{e1, e2}(::Type{FK5Coords{e1}}, ::Type{FK5Coords{e2}}) =
-    precess_from_j2000_capitaine(e1) * precess_from_j2000_capitaine(e2)'
+    precess_from_j2000(e1) * precess_from_j2000(e2)'
 
-
-# Identity transform
+# Scalar coordinate conversions
 convert{T<:AbstractSkyCoords}(::Type{T}, c::T) = c
-
 function convert{T<:AbstractSkyCoords, S<:AbstractSkyCoords}(::Type{T}, c::S)
     r = rotmat(T, S) * coords2cart(c)
     lon, lat = cart2coords(r)
     T(lon, lat)
 end
 
-# Vectorized forms, so that we only compute the rotation matrix once for FK5,
-# and apply it to the entire vector. The compiler doesn't seem to figure out
-# that rotmat(T, S) is a constant for FK5Coords{e} types.
+# Vector coordinate conversions
+# This is useful for FK5Coords so that we only compute the rotation
+# matrix (a function of epoch) once and apply it to the entire
+# vector. The compiler doesn't seem to figure out that rotmat(T, S) is
+# a constant for FK5Coords{e} types.
 convert{T<:AbstractSkyCoords,n}(::Type{Array{T,n}}, c::Array{T,n}) = c
-
 function convert{T<:AbstractSkyCoords, n, S<:AbstractSkyCoords}(
     ::Type{Array{T,n}}, c::Array{S, n})
     m = rotmat(T, S)

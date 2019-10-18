184c767fc95de6b601f9731ee7856e70e71d4b36
Miles Lucas <mdlucas@hawaii.edu>
2019-10-18 08:15:52 -1000

    rerun and update benchmarks

--- bench/bench.jl
+++ bench/bench.jl
@@ -11,20 +11,20 @@ io = open("julia_times.csv", "w")
 println(io, "n,system,time")
 for n in [1, 10, 100, 1000, 10_000, 100_000, 1_000_000]
     if n == 1
-        c = ICRSCoords(2pi*rand(), pi*(rand() - 0.5))
-        t1 = minimum(@benchmark(convert(GalCoords, $c)).times)/1e9
+        c = ICRSCoords(2pi * rand(), pi * (rand() - 0.5))
+        t1 = minimum(@benchmark(convert(GalCoords, $c)).times) / 1e9
         myprintln(io, "$n,galactic,$t1")
-        t2 = minimum(@benchmark(convert(FK5Coords{2000}, $c)).times)/1e9
+        t2 = minimum(@benchmark(convert(FK5Coords{2000}, $c)).times) / 1e9
         myprintln(io, "$n,fk5j2000,$t2")
-        t3 = minimum(@benchmark(convert(FK5Coords{1975}, $c)).times)/1e9
+        t3 = minimum(@benchmark(convert(FK5Coords{1975}, $c)).times) / 1e9
         myprintln(io, "$n,fk5j1975,$t3")
     else
-        c = [ICRSCoords(2pi*rand(), pi*(rand() - 0.5)) for i=1:n]
-        t1 = minimum(@benchmark(convert(Vector{GalCoords{Float64}}, $c)).times)/1e9
+        c = [ICRSCoords(2pi * rand(), pi * (rand() - 0.5)) for i = 1:n]
+        t1 = minimum(@benchmark(convert(Vector{GalCoords{Float64}}, $c)).times) / 1e9
         myprintln(io, "$n,galactic,$t1")
-        t2 = minimum(@benchmark(convert(Vector{FK5Coords{2000, Float64}}, $c)).times)/1e9
+        t2 = minimum(@benchmark(convert(Vector{FK5Coords{2000,Float64}}, $c)).times) / 1e9
         myprintln(io, "$n,fk5j2000,$t2")
-        t3 = minimum(@benchmark(convert(Vector{FK5Coords{1975, Float64}}, $c)).times)/1e9
+        t3 = minimum(@benchmark(convert(Vector{FK5Coords{1975,Float64}}, $c)).times) / 1e9
         myprintln(io, "$n,fk5j1975,$t3")
     end
     println()

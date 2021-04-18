9c844029039ffbfc82e29e5efc100cf44ef7047c
Miles Lucas <mdlucas@hawaii.edu>
2021-04-17 19:46:52 -0500

    update deployment again

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,13 @@ using SkyCoords
 DocMeta.setdocmeta!(SkyCoords, :DocTestSetup, :(using SkyCoords); recursive = true)
 
 makedocs(
-    sitename = "SkyCoords",
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    pages = ["Home" => "index.md", "API/Reference" => "api.md"],
     modules = [SkyCoords],
+    sitename = "SkyCoords.jl",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://juliaastro.github.io/SkyCoords.jl",
+    ),
+    pages = ["Home" => "index.md", "API/Reference" => "api.md"],
     strict = true,
 )
 
